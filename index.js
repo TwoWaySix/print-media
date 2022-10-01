4a2257ba06baa9825985d37ede5f6518768e013a
Circe <paz@twowaysix.com>
2022-10-01 19:55:35 +0200

    further automation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-ch1_1_div = document.getElementById("ch-1-1");
-let p = document.createElement("p");
-p.innerHTML = "Ahoy";
-ch1_1_div.appendChild(p);
-
-console.log("Test");
+data.forEach(d => {
+    let chapter = document.getElementById(d.class);
+    let p = document.createElement("p");
+    p.innerHTML = d.text;
+    chapter.appendChild(p);
+})
