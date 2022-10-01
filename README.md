643b3c13bc15e60345125ec05810939e2e04d8ab
Circe <paz@twowaysix.com>
2022-10-01 10:52:47 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # print-media
+
+## Setup
+```
+npm install -g pagedjs-cli pagedjs
+```
+
+## Create PDF
+```
+pagedjs-cli template.html -o result.pdf
+```
---
+++ index.js
@@ -0,0 +1,6 @@
+ch1_1_div = document.getElementById("ch-1-1");
+let p = document.createElement("p");
+p.innerHTML = "Ahoy";
+ch1_1_div.appendChild(p);
+
+console.log("Test");
